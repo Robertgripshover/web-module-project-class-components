76211d66998fedd50815043dbf663afeca398078
Robert Gripshover <robertgrips@gmail.com>
2022-08-25 13:49:24 -0400

    m

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,15 +1,43 @@
 import React from 'react'
 
 export default class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+
+      ]
+    }
+  } 
+  //when we are dealing with state we need a consturtor. 
+
+  //code below here is all static exept for the todos ul part, 
+  //to make that not static we use state
   render() {
+    const { todos } = this.state
+    console.log(todos)
+
     return (
       <div>
-        
+
         <h1>Todos</h1>
         <ul>
-          <li>brush Stella teeth</li>
-          <li>Make dinner</li>
-          <li>work out</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} { todo.completed?<span>- completed</span> : <span></span>}</li>)
+            })//this is maping over the todos array and making an individual "todo" for each item
+          }
         </ul>
 
         <form>

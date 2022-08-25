7e9c1bf4228f3b458887dcf29c27273e03ecd6ce
Robert Gripshover <robertgrips@gmail.com>
2022-08-25 13:35:35 -0400

    m

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        
+        <h1>Todos</h1>
+        <ul>
+          <li>brush Stella teeth</li>
+          <li>Make dinner</li>
+          <li>work out</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
+       
       </div>
     )
   }
